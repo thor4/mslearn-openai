052d2fad9e433afc04d3b518881efbab330a5a86
thor4 <6412103+thor4@users.noreply.github.com>
2024-04-02 10:39:49 -0400

    finish lab for exercise 2

--- Labfiles/02-azure-openai-api/Python/test-openai-model.py
+++ Labfiles/02-azure-openai-api/Python/test-openai-model.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Add Azure OpenAI package
+from openai import AzureOpenAI
 
 
 def main(): 
@@ -15,7 +16,20 @@ def main():
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
         
         # Initialize the Azure OpenAI client...
+        client = AzureOpenAI(
+            azure_endpoint = azure_oai_endpoint,
+            api_key = azure_oai_key,
+            api_version="2024-02-15-preview"
+        )
+
+        system_message = """I am a hiking enthusiast named Forest who helps people discover hikes in their area.
+            If no area is specified, I will default to near Rainier National Park. 
+            I will then provide three suggestions for nearby hikes that vary in length. 
+            I will also share an interesting fact about the local nature on the hikes when making a recommendation.
+            """
         
+        # Initialize messages list
+        messages_array = [{"role": "system", "content": system_message}]
 
 
         while True:
@@ -30,7 +44,18 @@ def main():
             print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
             
             # Add code to send request...
-            
+            # Send request to Azure OpenAI model
+            messages_array.append({"role": "user", "content": input_text})
+            response = client.chat.completions.create(
+                model = azure_oai_deployment,
+                temperature = 0.7,
+                max_tokens = 1200,
+                messages = messages_array
+            )
+            generated_text = response.choices[0].message.content
+
+            # Print the response
+            print("Summary: " + generated_text + "\n")
             
             
 
